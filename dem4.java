84e99ab73e1ee2cf4352100f3e0bab4df783ba36
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-02 12:19:37 +0530

    Add files via upload

---
+++ dem4.java
@@ -0,0 +1,20 @@
+
+class dem4
+{
+    // Prints numbers from 1 to n
+    static void printNos(int n)
+    {
+        if(n > 0)
+        {
+            printNos(n - 1);
+            System.out.print(n + " ");
+        }
+        return;
+    }
+
+    // Driver Code
+    public static void main(String[] args) 
+    {
+        printNos(100);
+    }
+}
