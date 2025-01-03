d45e222baf99c0dd911280ba2976058c9bdac304
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-03 10:09:50 +0530

    Add files via upload

---
+++ dem6.java
@@ -0,0 +1,10 @@
+public class dem6 {
+    public static void main(String[]args) {
+        String Username = "Ramyasree";
+        String pwd = "Ramyasree@123";
+        while(Username == "Ramyasree" && pwd == "Ramyasree@123"){
+            System.out.println("Hello Ramyasree");
+            break;
+        }
+    }
+}
