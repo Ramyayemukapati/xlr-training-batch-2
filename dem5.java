ed8e85468d855ed4d9ac4bb848e1fd919700ad4f
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-03 10:01:32 +0530

    Add files via upload

---
+++ dem5.java
@@ -0,0 +1,14 @@
+public class dem5 {
+    public static void main(String[]args) {
+        String Username = "User001";
+        String pwd = "User_001";
+        while(Username == "User001" && pwd == "User_001"){
+            System.out.println("Hello dear happy new year");
+            break;
+        }
+    }
+}
+
+
+
+
