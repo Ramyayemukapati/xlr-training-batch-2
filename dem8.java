5cd92805ba0d47f48c7ff5570a30bf02f6814d57
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-04 11:23:53 +0530

    Add files via upload

---
+++ dem8.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class dem8 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String loginid = " ";
+        String pwd = " ";
+        String reply = " ";
+        do{ 
+            
+        System.out.println("welcome to Malla Reddy university");
+        System.out.println("have you visited our site earlier");
+        reply = sc.nextLine();
+        if(reply == "no"){
+
+        System.out.println("login here");
+        System.out.println("enter ur id and pwd");
+        loginid = sc.nextLine();
+        pwd = sc.nextLine();
+        }else
+        System.out.println("you are you valued customer");
+        } while(loginid == "Ramya" && pwd == "12345");
+    }
+}
+
+
+    
+ 
+        
+        
+
+
+
+
+
+
