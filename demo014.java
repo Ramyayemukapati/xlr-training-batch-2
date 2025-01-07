c031ec138268c5ffc91ae035748fbe92b84f3b1a
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-07 10:16:45 +0530

    Add files via upload

---
+++ demo014.java
@@ -0,0 +1,29 @@
+public class demo014 {
+    public static void main(String[] args) {
+        int a = 5;
+
+        
+        System.out.println("Pre-Increment:");
+        System.out.println("Before: x = " + a); 
+        System.out.println("++x = " + (++a));  
+        System.out.println("After: x = " + a); 
+
+        
+        System.out.println("\nPost-Increment:");
+        System.out.println("Before: x = " + a); 
+        System.out.println("x++ = " + (a++));  
+        System.out.println("After: x = " + a); 
+
+        
+        System.out.println("\nPre-Decrement:");
+        System.out.println("Before: x = " + a); 
+        System.out.println("--x = " + (--a));  
+        System.out.println("After: x = " + a); 
+
+        
+        System.out.println("\nPost-Decrement:");
+        System.out.println("Before: x = " + a); 
+        System.out.println("x-- = " + (a--));  
+        System.out.println("After: x = " + a); 
+    }
+}
