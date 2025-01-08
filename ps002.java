18a4f1d9ae5b158bbdef39fd2faf129bdc6b6c65
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-08 10:04:27 +0530

    Add files via upload

---
+++ ps002.java
@@ -0,0 +1,9 @@
+public class ps002 {
+    public static void main(String[] args) {
+        int limit = 1000; 
+
+        for (int i = 100; i <= limit; i += 100) {
+            System.out.println(i);
+        }
+    }
+}
