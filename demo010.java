c169bfa0ee513c7316e260a5082e67f5c481d5c1
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-09 11:52:25 +0530

    Add files via upload

---
+++ demo010.java
@@ -0,0 +1,36 @@
+public class demo010 {
+    
+    public static String getFizzBizz(int input) {
+        
+        if (input <= 0) {
+            return "Error";
+        }
+        
+       
+        if (input % 3 == 0 && input % 5 == 0) {
+            return "FIZZBIZZ";
+        }
+        
+        
+        if (input % 3 == 0) {
+            return "FIZZ";
+        }
+        
+        
+        if (input % 5 == 0) {
+            return "BIZZ";
+        }
+    
+        return Integer.toString(input);
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(getFizzBizz(33));  
+        System.out.println(getFizzBizz(5));   
+        System.out.println(getFizzBizz(15));  
+        System.out.println(getFizzBizz(7));   
+        System.out.println(getFizzBizz(0));   
+        System.out.println(getFizzBizz(-3));  
+    }
+}
