3a818d4d57f526d668207cfecaa3f16ad5ab24f9
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-26 20:57:25 +0530

    Update and rename demo017.java to Demo017.java

--- Demo017.java
+++ Demo017.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class demo017 {
+public class Demo017 {
     public static void main(String[] args) {
-        // Create a scanner object to take input from the user
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a two-digit number: ");
         int number = scanner.nextInt();
-        int tens = number / 10;  // Extract the tens digit
-        int ones = number % 10;  // Extract the ones digit
+        int tens = number / 10; 
+        int ones = number % 10;  
         int sum = tens + ones;
        System.out.println(tens + " + " + ones + " = " + sum);
        scanner.close();
