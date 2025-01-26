d3f0190a2ee3d428f87c383c6412b0b36d07ddc4
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-26 20:50:20 +0530

    Update and rename demo012.java to Demo012.java

--- Demo012.java
+++ Demo012.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo012 {
+public class Demo012 {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
