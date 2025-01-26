e75cf12453edaa5d1b3687697d3a2a294762f4ff
Ramyayemukapati <ramyayemukapati16@gmail.com>
2025-01-26 20:47:27 +0530

    Update and rename dem8.java to Demo8.java

--- Demo8.java
+++ Demo8.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class dem8 {
+public class Demo8 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         String loginid = " ";
